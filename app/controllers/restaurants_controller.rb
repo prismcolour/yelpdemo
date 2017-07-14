f66d70147349e6c972d66aa9985147a57eb029bf
Christine Tang <prismcolour@icloud.com>
2017-07-14 08:23:45 -0400

    Put reviews on restaurant page - check reviews.blank first

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -9,7 +9,10 @@ class RestaurantsController < ApplicationController
 
   # GET /restaurants/1
   # GET /restaurants/1.json
-  def show; end
+  def show
+    # Accesses foreign-key from set_restaurant method
+    @reviews = Review.where(restaurant_id: @restaurant.id)
+  end
 
   # GET /restaurants/new
   def new
