3ffcfc4033989c499952e97dfbcfbc49bff43298
Christine Tang <prismcolour@icloud.com>
2017-07-15 10:09:23 -0400

    Set user permissions for review controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,7 +1,8 @@
 class ReviewsController < ApplicationController
-  before_action :authenticate_user!
   before_action :set_restaurant
   before_action :set_review, only: [:edit, :update, :destroy]
+  before_action :authenticate_user!
+  before_action :check_user, only: [:edit, :update, :destroy]
 
   # GET /reviews/new
   def new
@@ -49,7 +50,7 @@ class ReviewsController < ApplicationController
   def destroy
     @review.destroy
     respond_to do |format|
-      format.html { redirect_to reviews_url, notice: 'Review was successfully destroyed.' }
+      format.html { redirect_to restaurant_path(@restaurant), notice: 'Review was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
@@ -65,6 +66,12 @@ class ReviewsController < ApplicationController
   def review_params
     params.require(:review).permit(:rating, :comment)
   end
+
+  def check_user
+      unless (@review.user == current_user) || current_user.admin?
+        redirect_to root_url, alert: 'Sorry, this review belongs to someone else'
+      end
+    end
 
   # Sets restaurant instance through query string param
   # Used across controller actions when before_action filter is applied
